1caafdd9a4f504c91b74d9d3a58e9cbfef729e2d
zhenyu bai <z.bai@nus.edu.sg>
2023-07-05 14:58:11 +0800

    GrayScale version OK

---
+++ test/get_grayscale_img.py
@@ -0,0 +1,52 @@
+import serial
+import numpy as np
+from PIL import Image
+import sys
+
+ser = serial.Serial("/dev/ttyACM0",115200)
+img = bytes()
+
+
+# sizes
+height = 40 
+width = 40
+
+while True:
+    linein = ser.readline()
+
+    if linein == b'IMG\n' :
+        print("starting image transfer")
+        while True:
+            linein = ser.readline()
+            # if linein == b'END\n' :
+            #     break
+            # else :
+            img += linein
+            if len(img) >= height*width:
+                print ("len(img) = ", len(img))
+                break
+        print("transfer complete, len = ", len(img))
+        if (len(img) > height*width):
+            print("buffer data bigger than the image, delete the rest (usually useless)...")
+            img = img[:height*width]
+        break
+    print(linein)
+
+
+# Convert the byte array to a numpy array of uint16
+pixels = np.frombuffer(img, dtype=np.uint8)#.byteswap()
+
+# Reshape the numpy array to match the image dimensions
+pixels = pixels.reshape(height, width)
+
+# Create an image from the numpy array
+image = Image.fromarray(pixels, 'L')
+
+# save
+filename = "/tmp/gest.bmp"
+if len(sys.argv) < 2:
+    print("no filename specified, using default: /tmp/gest.bmp")
+else:
+    filename = sys.argv[1]
+print("saving image to ", filename)
+image.save(filename)
